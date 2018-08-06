a1e3ed2cf17521208eed483206c1482a4b7775c9
kcliu7 <874304201@qq.com>
2018-08-06 10:46:04 +0800

    t

---
+++ LC354.h
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxEnvelopes(vector<pair<int, int>>& envelopes) {
+        int res = 0, n = envelopes.size();
+        vector<int> dp(n, 1);
+        sort(envelopes.begin(), envelopes.end());
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (envelopes[i].first > envelopes[j].first && envelopes[i].second > envelopes[j].second) {
+                    dp[i] = max(dp[i], dp[j] + 1);
+                }
+            }
+            res = max(res, dp[i]);
+        }
+        return res;
+    }
+};
